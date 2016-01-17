4c1d2ff32b44953159d65cf4dd7bd2eac4470999
Dawid Myslak <dawid.myslak@gmail.com>
2016-01-17 00:38:32 +0000

    Ignore JavaScript files in project and git, hello-world example

---
+++ hello-world/app/boot.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser'
+import {HelloWorldComponent} from './hello-world.component';
+
+bootstrap(HelloWorldComponent);
---
+++ hello-world/app/hello-world.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'hello-world',
+    template: '<h1>Hello World!</h1>'
+})
+export class HelloWorldComponent { }
